012d1143f5b83bc45a54047fe667d8be53f925c6
phoebe <phoebe@womp.gay>
2025-06-29 03:01:17 +1000

    You can't fight the Homestuck.

--- nepeta.js
+++ nepeta.js
@@ -2,11 +2,17 @@
 // Technically this does more than just make nepeta follow your cursor, but it's primarily nepeta.
 // You can't fight the Homestuck.
 
-var nepeta = document.querySelector(".nepeta")
-var nepeta_follow = false;
+let nepeta = document.querySelector(".nepeta");
+let style = document.querySelector("#style");
+let homestuck = document.querySelector("#homestuck");
+let nepetaToggler = document.querySelector("#nepetaToggler");
+let nepeta_follow = false;
 
-var x = 0;
-var y = 0;
+let styleToggle = false;
+let styleToggleValue = 0;
+let styleToggler = false;
+let x = 0;
+let y = 0;
 
 function slide(_, top, left) {
     if(top) _.style.top = top+"px";
@@ -23,3 +29,26 @@ function toggleNepeta() {
     nepeta_follow = !nepeta_follow;
     nepeta.style.display = nepeta_follow ? "block" : "none"
 }
+window.onload = function() {
+    nepetaToggler.addEventListener("mouseover", function() {
+        styleToggler = true;
+    })
+    nepetaToggler.addEventListener("mouseleave", function() {
+        styleToggler = false;
+    })
+
+    setInterval(() => {
+        if(styleToggler) {
+            if(styleToggleValue === 2) {
+                styleToggle = !styleToggle;
+                homestuck.disabled = !styleToggle;
+                element.src = styleToggle ? "/images/wompstuck.png" : "/logo.svg";
+                tag.innerText = styleToggle ? "I WARNED YOU ABOUT THE STAIRS BRO!!!!" : possible_taglines[Math.floor(Math.random() * possible_taglines.length)];
+            }
+            styleToggleValue++;
+        } else {
+            styleToggleValue = 0;
+        }
+    }, 500);
+}
+
